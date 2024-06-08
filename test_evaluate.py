e020cfe1480e1bf997b27ff5ab1a7847c4e4e7cc
Yilei Wu <42246745+yilei-wu@users.noreply.github.com>
2024-06-08 22:58:07 +0800

    fix test_evaluate.py

--- test_evaluate.py
+++ test_evaluate.py
@@ -5,9 +5,15 @@ from evaluate import get_recall_and_precision, get_dice, get_avd
 
 def load_and_evaluate(ground_truth, prediction):
     # Load synthetic data
-    prediction = nib.load(ground_truth).get_fdata()
-    ground_truth = nib.load(prediction).get_fdata()
+    print(f"Ground truth path: {ground_truth_path}")
+    print(f"Prediction path: {prediction_path}")
 
+    if '\x00' in ground_truth_path or '\x00' in prediction_path:
+        raise ValueError("File path contains a null character")
+
+    ground_truth = nib.load(ground_truth_path).get_fdata()
+    prediction = nib.load(prediction_path).get_fdata()
+    
     binarize = lambda x: (x > 0).astype(np.uint8)
     prediction = binarize(prediction)
     ground_truth = binarize(ground_truth)
@@ -21,4 +27,14 @@ def load_and_evaluate(ground_truth, prediction):
     print("Dice:", dice)
     print("AVD:", avd)
 
-load_and_evaluate()
+if __name__ == "__main__":
+
+    ground_truth_path = "/Users/yileiwu/EPVS_challenge/labelsTr/EPVSCHALLENGE_ED_01_05.nii.gz"
+    prediction_path = "/Users/yileiwu/EPVS_challenge/validation_2d_3/EPVSCHALLENGE_ED_01_05.nii.gz"
+    load_and_evaluate(ground_truth_path, prediction_path)
+    # Ground truth path: /Users/yileiwu/EPVS_challenge/labelsTr/EPVSCHALLENGE_ED_01_05.nii.gz
+    # Prediction path: /Users/yileiwu/EPVS_challenge/validation_2d_3/EPVSCHALLENGE_ED_01_05.nii.gz
+    # Recall: 0.6216216216216216
+    # Precision: 0.5985130111524164
+    # Dice: 0.5603349828701941
+    # AVD: 0.16436663233779608
